b266267d471e3086e432aa6a32606a9f3fef1be6
Carlos Villatoro <crvb0797@gmail.com>
2020-12-28 13:42:42 -0600

    terminando con el proyecto ya con estilos

---
+++ src/utils/resolveRoutes.js
@@ -0,0 +1,9 @@
+const resoleRoutes = (route) => {
+  if (route.length <= 3) {
+    let validRoute = route === "/" ? route : "/:id";
+    return validRoute;
+  }
+  return `/${route}`;
+};
+
+export default resoleRoutes;
